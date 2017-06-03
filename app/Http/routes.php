f6f54e463161cf58300def864307b273dcc3cc25
国宝零号 <244774097@qq.com>
2017-06-03 21:54:13 +0800

    ssdsd

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,9 +6,10 @@
 Route::post('resetPassword', 'SmsController@resetPassword');//重置密码
 Route::post('bindPhone', 'SmsController@bindPhone');//手机绑定
 Route::post('authPhone', 'AuthenticateController@phoneAuth');//手机号码登录
+Route::get('getSmsCode', 'SmsController@getSmsCode');
 
 Route::group(['middleware' => 'throttle:3'], function ($api) {
-    Route::get('getSmsCode', 'SmsController@getSmsCode');
+
 });
 Route::group(['middleware' => 'throttle:3'], function ($api) {
 
